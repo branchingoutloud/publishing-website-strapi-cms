ad7ff766c115906e5e55021a34cf7b444e22a59c
branchingoutloud <pranav.sonawane@pyrack.com>
2025-09-04 16:09:57 +0530

    findOne, findMany

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -7,50 +7,109 @@ import { factories } from "@strapi/strapi";
 export default factories.createCoreController(
   "api::product.product",
   ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const result = await strapi.documents("api::product.product").findOne({
+        documentId: id,
+        populate: {
+          vendor: true,
+          product_logo: true,
+          category_reference: true,
+          product_reviews: {
+            populate: {
+              user_id: true,
+            },
+          },
+          product_feature_values: {
+            populate: {
+              feature_id: true,
+            },
+          },
+        },
+      });
+      return result;
+    },
     async find(ctx) {
       // Call the default find method
-      const { data, meta } = await super.find(ctx);
+      // const { data, meta } = await super.find(ctx);
+
+      // let features = await strapi.documents("api::product-feature-value.product-feature-value").findMany({
+      //   filters: {
+      //     product: { $in: data.map((product: any) => product.id) } as any
+      //   },
+      //   populate: {
+      //     feature_id: true,
+      //   },
+      // });
+
+      // console.log(features, "features");
 
       // Transform each product
-      const transformedData = data.map((product) => ({
-        ...product,
-        pros_and_cons: {
-          pros_summary: this.extractSummaryPoints(product.pros),
-          pros: [
-            "Real-time collaboration",
-            "Robust integrations",
-            "User-friendly interface",
-            "Scalable",
-            "Cost-effective",
-            "Cross-platform",
-            "Secure",
-            "Customizable",
-            "Easy to use",
-            "Fast",
-          ],
-          cons_summary: this.extractSummaryPoints(product.cons),
-          cons: [
-            "Difficult to use",
-            "Expensive",
-            "Not secure",
-            "Not customizable",
-            "Not scalable",
-            "Not cross-platform",
-            "Not robust",
-            "Not fast",
-            "Not easy to use",
-            "Not cost-effective",
-          ],
-        },
-      }));
+      // const transformedData = data.map((product: any) => ({
+      //   ...product,
+      //   pros_and_cons: {
+      //     pros_summary: this.extractSummaryPoints(product.pros),
+      //     pros: [
+      //       "Real-time collaboration",
+      //       "Robust integrations",
+      //       "User-friendly interface",
+      //       "Scalable",
+      //       "Cost-effective",
+      //       "Cross-platform",
+      //       "Secure",
+      //       "Customizable",
+      //       "Easy to use",
+      //       "Fast",
+      //     ],
+      //     cons_summary: this.extractSummaryPoints(product.cons),
+      //     cons: [
+      //       "Difficult to use",
+      //       "Expensive",
+      //       "Not secure",
+      //       "Not customizable",
+      //       "Not scalable",
+      //       "Not cross-platform",
+      //       "Not robust",
+      //       "Not fast",
+      //       "Not easy to use",
+      //       "Not cost-effective",
+      //     ],
+      //   },
+      // }));
+      // return { data: data, meta };
 
-      return { data: transformedData, meta };
+      const { pagination, filters, sort } = ctx.query;
+      const safePagination =
+        pagination && typeof pagination === "object" ? pagination : {};
+
+      const results = await strapi.documents('api::product.product').findMany({
+        filters,
+        sort,
+        pagination,
+        populate: {
+          vendor: true,
+          product_logo: true,
+          // category_reference: true,
+          // product_feature_values: {
+          //   populate: {
+          //     feature_id: true,
+          //   },
+          // },
+        },
+      });
+
+      // If you need meta for pagination, you can
+      const meta = await strapi.documents('api::product.product').count({ filters });
+
+      return { data: results, meta: { pagination: { ...safePagination, total: meta } } };
+
     },
 
     extractSummaryPoints(text) {
       // How do you want to split the text into points?
       return text ? text.split(",").map((point) => point.trim()) : [];
     },
+
     async compareProducts(ctx) {
       const { productIds } = ctx.query;
 
@@ -66,10 +125,8 @@ export default factories.createCoreController(
       // .map(Number);
       console.log(ids, "ids");
 
-      let products = await strapi.entityService.findMany(
-        "api::product.product",
-        {
-          where: { documentId: { $in: ids } },
+      let products = await strapi.documents("api::product.product").findMany({
+          filters: { documentId: { $in: ids } },
           populate: {
             vendor: true,
             product_logo: true,
@@ -79,8 +136,8 @@ export default factories.createCoreController(
               },
             },
           },
-        }
-      );
+        });
+
       products = products?.map((product: any) => {
         let features = {};
         product?.product_feature_values?.map((feature: any) => {
