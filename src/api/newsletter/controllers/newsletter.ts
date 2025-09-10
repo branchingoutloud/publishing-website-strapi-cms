2d2ff82250d142310f29700f2b3ba8032064d9b8
branchingoutloud <pranav.sonawane@pyrack.com>
2025-09-10 16:06:52 +0530

    newsletter find and findone methods

--- src/api/newsletter/controllers/newsletter.ts
+++ src/api/newsletter/controllers/newsletter.ts
@@ -4,4 +4,31 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::newsletter.newsletter');
+export default factories.createCoreController(
+    'api::newsletter.newsletter',
+    ({ strapi }) => ({
+        async findOne(ctx) {
+            const { id } = ctx.params;
+            const result = await strapi.documents("api::newsletter.newsletter").findOne({
+                documentId: id,
+            });
+
+            return result;
+        },
+        async find(ctx) {
+            const { pagination, filters, sort } = ctx.query;
+            const safePagination = pagination && typeof pagination === "object" ? pagination : {};
+
+            const result = await strapi.documents("api::newsletter.newsletter").findMany({
+                filters,
+                sort,
+                pagination,
+            });
+
+            // If you need meta for pagination, you can
+            const meta = await strapi.documents('api::newsletter.newsletter').count({ filters });
+
+            return { data: result, meta: { pagination: { ...safePagination, total: meta } } };
+        },
+    })
+)
